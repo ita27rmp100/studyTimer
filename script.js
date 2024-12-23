f8ccb7b514b7bef32d3760627d7ed24e92300458
Abderahim <ita27rmp100@gmail.com>
2024-12-23 10:58:32 +0100

    complete the html/css settings and start the js coding

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 // change timer mode
 let modesID = ['study','lbreak','sbreak']
+let mode = 'study'
 function chmod(id){
+    $('#counter').text(localStorage.getItem(id));
+    mode = id
     for(i=0;i<modesID.length;i++){
         if(id==modesID[i]){
-            $(`#${id}`).addClass('btn-light')
+            $(`#${id}`).addClass('btn-outline-dark')
             $(`#${id}`).removeClass('btn-dark')
         }
         else{
             $(`#${modesID[i]}`).addClass('btn-dark')
-            $(`#${modesID[i]}`).removeClass('btn-light')
+            $(`#${modesID[i]}`).removeClass('btn-outline-dark')
         }
     }
     
 }
+// stop / play the counter
+let stopPlayArr = ['play','restart']
+let play = 0 
+function playStop(){
+    play = Math.abs(play-1);
+    
+}
+
+// restart
+function restart(){
+    $("#counter").text("00:00")
+}
+// settings (change the timer start)
+$(document).ready(
+    function() {
+        $('#counterRange').on('input', function() {
+            let counterVal = `${$(this).val()}:00`
+            $('#counter').text(counterVal);
+            localStorage.setItem(mode,`${$(this).val()}:00`)
+        });
+    }
+)
